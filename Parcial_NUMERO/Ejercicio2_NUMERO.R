9413fa7ef3b6cfdd91c177479b6f137bbb6a7d49
nysdan <29280983+nysdan@users.noreply.github.com>
2021-10-13 00:20:21 -0300

    uo

--- Parcial_NUMERO/Ejercicio2_NUMERO.R
+++ Parcial_NUMERO/Ejercicio2_NUMERO.R
@@ -3,20 +3,19 @@
 
 # Ejercicio 2 -------------------------------------------------------------
 
-# Análisis de la secuencia en R: Utilice RStudio
+# Análisis de la secuencia en R: Utiliza RStudio
 
 
 # Pregunta 2.a ------------------------------------------------------------
 
-# Importe las secuencias contenidas en el archivo bicho.fasta, guardándolas como un objeto en R. Describa y fundamente cada paso.
-
+ # Importa las secuencias contenidas en el archivo bicho.fasta, guardándolas como un objeto en R. Describe y fundamenta cada paso. (3 puntos)
 
 ## Respuesta 
 
 
 # Pregunta 2.b ------------------------------------------------------------
 
-# ¿Qué tipo de objeto creó en el punto anterior? ¿Cómo lo averiguó?
+#	¿Qué tipo de objeto se creó en el punto anterior? ¿Cómo lo averiguaste? (2 puntos)
 
 
 ## Respuesta
@@ -24,35 +23,35 @@
 
 # Pregunta 2.c ------------------------------------------------------------
 
-# ¿Cuántas secuencias contiene el objeto? Describa los comandos utilizados.
+# ¿Cuántas secuencias contiene el objeto? Describe los comandos utilizados. (3 puntos)
 
 ## Respuesta
 
 
 # Pregunta 2.d ------------------------------------------------------------
 
-# Construya un vector con los largos de todas las secuencias. Describa los comandos utilizados.
+# d.	Construye un vector con los largos de todas las secuencias. Describe los comandos utilizados. (5 puntos)
 
 ## Respuesta
 
 
 # Pregunta 2.e ------------------------------------------------------------
 
-# ¿Cuánto mide la más larga y cuánto la más corta? Fundamente.
+# ¿Cuánto mide la más larga y cuánto la más corta? Fundamenta. (2 puntos)
 
 ## Respuesta
 
 
 # Pregunta 2.f ------------------------------------------------------------
 
-# Traduzca las secuencias.
+# Guarda las secuencias traducidas en otro objeto. (3 puntos)
 
 ## Respuesta
 
 
 # Pregunta 2.g ------------------------------------------------------------
 
-# ¿Cuál es el aminoácido más frecuente? Considere para esto la secuencia total, sin distinguir entre CDS.
+# ¿Cuál es el aminoácido más frecuente? Considera para esto la secuencia total, sin distinguir entre CDS. (5 puntos)
 
 ## Respuesta
 
