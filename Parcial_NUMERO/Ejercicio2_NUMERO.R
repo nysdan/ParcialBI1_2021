025eaf11090d595bb3c14b333def8bff67316f61
nysdan <29280983+nysdan@users.noreply.github.com>
2021-10-12 23:58:39 -0300

    varios

---
+++ Parcial_NUMERO/Ejercicio2_NUMERO.R
@@ -0,0 +1,59 @@
+# Parcial Bioinformática 1 2021
+
+
+# Ejercicio 2 -------------------------------------------------------------
+
+# Análisis de la secuencia en R: Utilice RStudio
+
+
+# Pregunta 2.a ------------------------------------------------------------
+
+# Importe las secuencias contenidas en el archivo bicho.fasta, guardándolas como un objeto en R. Describa y fundamente cada paso.
+
+
+## Respuesta 
+
+
+# Pregunta 2.b ------------------------------------------------------------
+
+# ¿Qué tipo de objeto creó en el punto anterior? ¿Cómo lo averiguó?
+
+
+## Respuesta
+
+
+# Pregunta 2.c ------------------------------------------------------------
+
+# ¿Cuántas secuencias contiene el objeto? Describa los comandos utilizados.
+
+## Respuesta
+
+
+# Pregunta 2.d ------------------------------------------------------------
+
+# Construya un vector con los largos de todas las secuencias. Describa los comandos utilizados.
+
+## Respuesta
+
+
+# Pregunta 2.e ------------------------------------------------------------
+
+# ¿Cuánto mide la más larga y cuánto la más corta? Fundamente.
+
+## Respuesta
+
+
+# Pregunta 2.f ------------------------------------------------------------
+
+# Traduzca las secuencias.
+
+## Respuesta
+
+
+# Pregunta 2.g ------------------------------------------------------------
+
+# ¿Cuál es el aminoácido más frecuente? Considere para esto la secuencia total, sin distinguir entre CDS.
+
+## Respuesta
+
+
